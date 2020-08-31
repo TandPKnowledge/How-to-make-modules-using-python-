90316917b5ed90c9efa11c3b96150f1897a73907
T and P Knowledge <70309977+TandPKnowledge@users.noreply.github.com>
2020-08-31 15:07:35 +0530

    Add files via upload

---
+++ How to make modules using python/areas.py
@@ -0,0 +1,10 @@
+import math
+
+def triangle(base, height):
+    return (base*height)/2
+
+def rectangle(base, heigth):
+    return base*heigth
+
+def circle(radius):
+    return math.pi*(radius**2)
---
+++ How to make modules using python/calcu.py
@@ -0,0 +1,7 @@
+import areas
+
+# print(dir(areas))
+
+print(areas.triangle(7,3))
+print(areas.circle(5))
+print(areas.rectangle(5,6))
